ca81cba1aa563690ad5df0523e58ba76293c8ade
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-10-01 15:33:56 -0700

    file formating: fix to display properly in GitHub

--- algorithms/greedy.py
+++ algorithms/greedy.py
@@ -1,1 +1,15 @@
-from algorithms.wikiracer import WikiRacerclass Greedy(WikiRacer):    def __init__(self, max_path_length, doc_sim_func):        """        Parameters        ----------        max_path_length : int        doc_sim_func : function            Takes two wikipedia page titles and returns            some measure of their similarity.        """        super().__init__(max_path_length)        self.doc_sim = doc_sim_func
+from algorithms.wikiracer import WikiRacer
+
+
+class Greedy(WikiRacer):
+    def __init__(self, max_path_length, doc_sim_func):
+        """
+        Parameters
+        ----------
+        max_path_length : int
+        doc_sim_func : function
+            Takes two wikipedia page titles and returns
+            some measure of their similarity.
+        """
+        super().__init__(max_path_length)
+        self.doc_sim = doc_sim_func
