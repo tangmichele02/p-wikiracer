077f575beec69ced688f18da84efe74cb43e2a35
Michele Tang <tangmichele02@gmail.com>
2023-11-14 22:27:55 -0800

    updated frontend

--- react-flask-app/api/app.py
+++ react-flask-app/api/app.py
@@ -1,21 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import time
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
 
 @app.route("/", methods=['GET', 'POST'])
 def send():
-    if request.method == 'POST':
-        start = request.form['start']
-        end = request.form['end']
-        if (start != "") and (end != ""):
-            path_val = path(start, end)
-            return render_template('output.html',path_val=path_val)
     return render_template('home.html')
 
-
-# will replace by importing backend function 
-def path(start, end):
-    return ("The final path is " + start + " to " + end)
-
+@app.route('/api')
+def api():
+    response = {'message': 'Hello, World!'}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8080)
+
+# from flask import Flask, render_template, request
+# app = Flask(__name__)
+
+# @app.route("/", methods=['GET', 'POST'])
+# def send():
+#     if request.method == 'POST':
+#         start = request.form['start']
+#         end = request.form['end']
+#         if (start != "") and (end != ""):
+#             path_val = path(start, end)
+#             return render_template('output.html',path_val=path_val)
+#     return render_template('home.html')
+
+
+# # will replace by importing backend function 
+# def path(start, end):
+#     return ("The final path is " + start + " to " + end)
+
+
+# if __name__ == '__main__':
+#     app.run(host = '0.0.0.0', port = 8080)
