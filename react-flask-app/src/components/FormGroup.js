a5e248d500d675bdde3ed9f73e75d944dccfe320
Michele Tang <tangmichele02@gmail.com>
2023-11-19 12:57:32 -0800

    connecting input output between front and backend

--- react-flask-app/src/components/FormGroup.js
+++ react-flask-app/src/components/FormGroup.js
@@ -1,14 +1,52 @@
+import { useState } from 'react';
 import './components.css'
 
+function handleClick(inputText, outputText) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ inputtext: inputText,
+                            outputtext: outputText})
+  };
+  fetch('http://127.0.0.1:5000/api/alg', requestOptions)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const response_value = response.json();
+      console.log(response_value);
+    });
+  // return (
+  //   <div>
+  //     <p>Hi</p>
+  //   </div>
+  // )
+}
+
+// function displayPath(response) {
+//   return (
+//     <div>
+//       <p>{response}</p>
+//     </div>
+//   );
+// }
+
 function FormGroup() {
+  const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
   return  (
-    <div class="form-group">
-        <div class="start-field">
-            <input type="text" name = "start"/>
-        </div>
-        <div class="end-field">
-            <input type="text" name = "end"/>
-        </div>
+    <div>
+      <div class="form-group">
+          <div class="start-field">
+              <input type="text" name = "start" onChange={(e) => setInputText(e.target.value)}/>
+          </div>
+          <div class="end-field">
+            <input type="text" name = "end" onChange={(e) => setOutputText(e.target.value)}/>
+          </div>
+      </div>
+      <div class = "submit-button">
+        <input class="btn" type="submit" value="submit" onClick={() => handleClick(inputText, outputText)}/>
+      </div>
     </div>
   );
 }
