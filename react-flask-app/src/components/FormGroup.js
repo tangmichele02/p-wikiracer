fb4cccdc6e4868196a10e92aa82ba7ba020c7be1
Michele Tang <tangmichele02@gmail.com>
2023-12-01 15:24:30 -0800

    reverting changes

--- react-flask-app/src/components/FormGroup.js
+++ react-flask-app/src/components/FormGroup.js
@@ -4,72 +4,60 @@ import React, { useState } from 'react';
 import { useHistory } from "react-router-dom"
 
 function FormGroup() {
-  // const [startVal, setStartVal] = useState("");
-  // const [endVal, setEndVal] = useState("");
-  // const [pathstr, setpathstr] = useState("");
+  const [startVal, setStartVal] = useState("");
+  const [endVal, setEndVal] = useState("");
+  const [pathstr, setpathstr] = useState("");
   let history = useHistory();
 
-  function handleClick() {
-    
-    history.push("/home");
-  }
+  function handleClick(inputText, outputText) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ inputtext: startVal,
+                              outputtext: endVal})
+    };
+    fetch('http://127.0.0.1:5000/api/alg', requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const response_value = response.json();
+        response_value
+          .then((value) => {
+            console.log(value);
+            setpathstr(value);
+          })
+        console.log(response_value);
+      });
 
-  return (
-    <button type="button" onClick={handleClick}>
-      Go home
-    </button>
-  );
-}
-
-//   function handleClick(inputText, outputText) {
-//     const requestOptions = {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ inputtext: startVal,
-//                               outputtext: endVal})
-//     };
-//     fetch('http://127.0.0.1:5000/api/alg', requestOptions)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         const response_value = response.json();
-//         response_value
-//           .then((value) => {
-//             console.log(value);
-//             setpathstr(value);
-//           })
-//         console.log(response_value);
-//       });
-
-//       history.push("/results");
+      history.push("/results");
       
-//     }
+    }
 
-//   return  (
-//     <div>
+  return  (
+    <div>
     
-//     <div class="form-group">
-//       {/* <AsyncAutocomplete title="Start Page" setValue={setStartVal}/>
-//       <AsyncAutocomplete title="End Page" setValue={setEndVal}/> */}
-//           <div class="start-field">
-//               <input type="text" name = "start" onChange={(e) => setStartVal(e.target.value)}/>
-//           </div>
-//           <div class="end-field">
-//             <input type="text" name = "end" onChange={(e) => setEndVal(e.target.value)}/>
-//           </div>
-//       </div>
-//       <div class = "submit-button">
-//         <input class="btn" type="submit" value="submit" onClick={() => handleClick(startVal, endVal)}/>
-//         <div>
-//           {startVal}
-//           {endVal}
-//           {pathstr}
-//         </div>
-//       </div>
-//     </div>
-//   );
+    <div class="form-group">
+      {/* <AsyncAutocomplete title="Start Page" setValue={setStartVal}/>
+      <AsyncAutocomplete title="End Page" setValue={setEndVal}/> */}
+          <div class="start-field">
+              <input type="text" name = "start" onChange={(e) => setStartVal(e.target.value)}/>
+          </div>
+          <div class="end-field">
+            <input type="text" name = "end" onChange={(e) => setEndVal(e.target.value)}/>
+          </div>
+      </div>
+      <div class = "submit-button">
+        <input class="btn" type="submit" value="submit" onClick={() => handleClick(startVal, endVal)}/>
+        <div>
+          {startVal}
+          {endVal}
+          {pathstr}
+        </div>
+      </div>
+    </div>
+  );
 
-// }
+}
 
 export default FormGroup;
