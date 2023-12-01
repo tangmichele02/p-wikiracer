f1d4ac60c46ce47967ddd981cc35c5b7e387ca8e
Michele Tang <tangmichele02@gmail.com>
2023-12-01 13:34:13 -0800

    displays output on main page

--- react-flask-app/src/components/FormGroup.js
+++ react-flask-app/src/components/FormGroup.js
@@ -1,36 +1,39 @@
 import './components.css'
 import React, { useState } from 'react';
 
-function handleClick(inputText, outputText) {
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ inputtext: inputText,
-                            outputtext: outputText})
-  };
-  fetch('http://127.0.0.1:5000/api/alg', requestOptions)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const response_value = response.json();
-      response_value
-        .then((value) => {
-          console.log(value);
-        })
-      console.log(response_value);
-    });
-  
-    return (
-      <div>
-        <p>Hi</p>
-      </div>
-    )
-}
-
 function FormGroup() {
   const [inputText, setInputText] = useState("");
   const [outputText, setOutputText] = useState("");
+  const [pathstr, setpathstr] = useState("");
+
+
+  function handleClick(inputText, outputText) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ inputtext: inputText,
+                              outputtext: outputText})
+    };
+    fetch('http://127.0.0.1:5000/api/alg', requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const response_value = response.json();
+        response_value
+          .then((value) => {
+            console.log(value);
+            setpathstr(value);
+          })
+        console.log(response_value);
+      });
+    
+      return (
+        <div>
+          <p>Hi</p>
+        </div>
+      )
+  }
 
   return  (
     <div>
@@ -45,9 +48,7 @@ function FormGroup() {
       <div class = "submit-button">
         <input class="btn" type="submit" value="submit" onClick={() => handleClick(inputText, outputText)}/>
         <div>
-          {
-            
-          }
+          {pathstr}
         </div>
       </div>
     </div>
