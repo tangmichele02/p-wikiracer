594a72aec7f2c2c928ca04ac2dba7c96dc162630
Michele Tang <tangmichele02@gmail.com>
2023-12-03 13:18:28 -0800

    removed cors

--- react-flask-app/src/components/FormGroup.js
+++ react-flask-app/src/components/FormGroup.js
@@ -1,60 +1,43 @@
-// import AsyncAutocomplete from './AsyncAutocomplete';
+import AsyncAutocomplete from './AsyncAutocomplete';
 import './components.css'
 import React, { useState } from 'react';
-// import { useHistory } from "react-router-dom"
+import axios from 'axios';
 
 function FormGroup() {
   const [startVal, setStartVal] = useState("");
   const [endVal, setEndVal] = useState("");
-  const [pathStr, setpathstr] = useState("");
-  // let history = useHistory();
+  const [pathstr, setpathstr] = useState("");
 
-  function handleClick(inputText, outputText) {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ inputtext: startVal,
-                              outputtext: endVal})
-    };
-    fetch('http://127.0.0.1:5000/api/alg', requestOptions)
+
+  function handleClick(startVal, endVal) {
+    axios.get("http://127.0.0.1:5000/api/alg?startValue=" + startVal + "&endValue=" + endVal)
       .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const response_value = response.json();
-        response_value
-          .then((value) => {
-            console.log(value);
-            setpathstr(value);
-          })
-        console.log(response_value);
+        setpathstr(response.data.path)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
       });
-
-      // history.push("/results");
-      return (
-        <div>
-          {pathStr}
-        </div>
-      )
     }
+    
 
   return  (
     <div>
-      <div class="form-group">
-        {/* <AsyncAutocomplete title="Start Page" setValue={setStartVal}/>
-        <AsyncAutocomplete title="End Page" setValue={setEndVal}/> */}
-            <div class="start-field">
-                <input type="text" name = "start" onChange={(e) => setStartVal(e.target.value)}/>
-            </div>
-            <div class="end-field">
-              <input type="text" name = "end" onChange={(e) => setEndVal(e.target.value)}/>
-            </div>
-        </div>
-        <div class = "submit-button">
-          <input class="btn" type="submit" value="submit" onClick={() => handleClick(startVal, endVal)}/>
-        </div>
+      
+    <div class="headers">
+      <AsyncAutocomplete title="Start Page" setValue={setStartVal}/>
+      <div class="input-separator"></div>
+      <AsyncAutocomplete title="End Page" setValue={setEndVal}/>
+    </div>
+
+    <div class = "submit-button">
+        <input class="btn" type="submit" value="submit" onClick={() => handleClick(startVal, endVal)}/>
+    </div>
+    <div>
+      {pathstr}
+    </div>
     </div>
   );
+
 
 }
 
